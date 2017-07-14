209ad8922f35982ba26d5d4427e33cd6ae4c4250
Adam O'Hern <adam@mechanicalcolor.com>
2017-07-14 14:02:20 -0400

    syntax bug

--- lxserv/ls_resetPrefs.py
+++ lxserv/ls_resetPrefs.py
@@ -8,7 +8,7 @@ class ResetPrefsCommandClass(lifesaver.CommanderClass):
     def commander_execute(self, msg, flags):
         clearCmd = 'lifesaver.clearPrefs'
         for i in lifesaver.KEEPERS:
-            clearCmd.append(str(lx.eval('lifesaver.preference %s ?' % i[0])))
+            clearCmd += (str(lx.eval('lifesaver.preference %s ?' % i[0])))
 
         lx.eval(clearCmd)
 
