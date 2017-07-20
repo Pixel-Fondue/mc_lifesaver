652026f172e121d2ecb4b89808baf06670b17dde
adamohern <adam@mechanicalcolor.com>
2017-07-20 09:06:04 -0400

    1.0.3 disable file system backups

--- lifesaver/Var.py
+++ lifesaver/Var.py
@@ -9,7 +9,7 @@ KEEPERS = [
          ['UserValues', 'User Values', True, 'ls_UserValues', True],
         #  ['HudCustomTools_Edit', 'HUD Customizations', True, 'ls_HudCustomTools_Edit', False],
         #  ['AttributeRecentsAndFrequents', 'Recent and Frequent Tools', True, 'ls_AttributeRecentsAndFrequents', False],
-         ['FileSystem', 'Recent Files and Folders', True, 'ls_FileSystem', False],
+        #  ['FileSystem', 'Recent Files and Folders', True, 'ls_FileSystem', False],
          ['CheckForUpdates', 'Check for Updates', True, 'ls_CheckForUpdates', False],
          ['PostUsageStats', 'Post Usage Stats', True, 'ls_PostUsageStats', False]
 ]
